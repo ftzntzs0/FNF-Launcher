ddec42f133fec55e289b87fc9dc06718db8e7610
ftz*! <68380939+feentzs@users.noreply.github.com>
2026-05-18 14:10:53 -0300

    Fix icon path format in MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -269,7 +269,7 @@ namespace FNFVsliceLauncher
                             string jsonContent = await File.ReadAllTextAsync(metaFilePath);
                             var meta = JsonSerializer.Deserialize<PolymodMeta>(jsonContent);
 
-                            string iconPath = "ms-appx:///Assets/no_preview.png";
+                            string iconPath = "ms-appx:///Assets/no-preview.png";
 
                             if (File.Exists(iconFilePath))
                             {
